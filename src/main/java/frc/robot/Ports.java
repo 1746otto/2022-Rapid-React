caafd8b525bdbf91458e8d730bafa74da504fa68
Josselyn Trejo <josselyntrejo1@gmail.com>
2022-01-22 13:44:36 -0500

    Changed CAN ID

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -3,9 +3,9 @@ package frc.robot;
 public class Ports {
 
 	//Shooter
-	public static final int SHOOTER_MASTER = 1;
-	public static final int SHOOTER_SLAVE_1 = 2;
-	public static final int SHOOTER_SLAVE_2 = 3;
-	public static final int SHOOTER_SLAVE_3 = 4;
+	public static final int SHOOTER_MASTER = 42;
+	public static final int SHOOTER_SLAVE_1 = 40;
+	public static final int SHOOTER_SLAVE_2 = 43;
+	public static final int SHOOTER_SLAVE_3 = 41;
 
 }
