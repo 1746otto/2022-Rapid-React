be3701170194209e545688cb88193330ed6d1b79
Josselyn Trejo <josselyntrejo1@gmail.com>
2022-01-22 14:22:00 -0500

    Made an Indexer

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -7,5 +7,6 @@ public class Ports {
 	public static final int SHOOTER_SLAVE_1 = 40;
 	public static final int SHOOTER_SLAVE_2 = 43;
 	public static final int SHOOTER_SLAVE_3 = 41;
+    public static final int INDEXER_MOTOR = 60;
 
 }
--- src/main/java/frc/robot/commands/ShooterCommand.java
+++ src/main/java/frc/robot/commands/ShooterCommand.java
@@ -35,7 +35,9 @@ public class ShooterCommand extends CommandBase {
   
     // Called once the command ends or is interrupted.
     @Override
-    public void end(boolean interrupted) {}
+    public void end(boolean interrupted) {
+      m_subsystem.setZeroPower();
+    }
   
     // Returns true when the command should end.
     @Override
