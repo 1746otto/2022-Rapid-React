3b2ba65898d86cf2a847b0d6ffbbc7a58a5a6919
Team OTTO Programming <programming@team1746.com>
2022-02-12 17:13:39 -0500

    Slight formatting fixes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,23 +6,18 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.Constants.AutonConstants;
 import frc.robot.commands.ArcadeDriveCommand;
-import frc.robot.commands.AutonBasic;
 import frc.robot.commands.AutonDriveCommand;
-import frc.robot.commands.IndexerCommand;
 import frc.robot.commands.IndexerFullForwardCommand;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IndexerSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.commands.ShooterCommand;
 import frc.robot.commands.ShooterFullPowerCommand;
 import frc.robot.commands.VisionDriveCommand;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.Vision;
 import frc.robot.Constants.ControllerConstants;
-import frc.robot.commands.ArcadeDriveCommand;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
