2db7b208d58de8164709255dc36e22844f68b011
rohanmnair <rohanmanunair@gmail.com>
2022-02-23 19:27:44 -0500

    Made basic CSV table

---
+++ src/main/java/frc/robot/CSVtable.java
@@ -0,0 +1,9 @@
+package frc.robot;
+import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.DriveSubsystem;
+import java.io.File;
+import java.io.FileWriter;
+
+public class CSVtable {
+    
+}
