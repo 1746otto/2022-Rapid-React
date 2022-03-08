b1a1e27f6e9e852b42711abd2d8fe2e53615914e
Arpit Mittal <arpit.mittal.2.71@gmail.com>
2022-03-08 18:32:46 -0500

    fixed indentation

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -37,92 +37,92 @@ import frc.robot.subsystems.Vision;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-    private final XboxController m_controller = new XboxController(ControllerConstants.kport);
-    // The robot's subsystems and commands are defined here...
-    private final DriveSubsystem m_driveSubsystem = new DriveSubsystem();
-    private final ShooterSubsystem m_shooterSubsystem = new ShooterSubsystem();
-    private final ClimberSubsystem m_climberSubsystem = new ClimberSubsystem();
-    private final Vision m_visionSubsystem = new Vision();
-    private final VisionDriveCommand m_visionDriveCommand =
-            new VisionDriveCommand(m_driveSubsystem, m_controller, m_visionSubsystem);
-    private final IndexerSubsystem m_indexerSubsystem = new IndexerSubsystem();
-    private final IntakeSubsystem m_intakeSubsystem = new IntakeSubsystem();
-    private final Compressor compressor =
-            new Compressor(RobotConstants.kREVPH, PneumaticsModuleType.REVPH);
-    VisionTuningCommand m_visionTuningCommand = new VisionTuningCommand(m_visionDriveCommand);
+  private final XboxController m_controller = new XboxController(ControllerConstants.kport);
+  // The robot's subsystems and commands are defined here...
+  private final DriveSubsystem m_driveSubsystem = new DriveSubsystem();
+  private final ShooterSubsystem m_shooterSubsystem = new ShooterSubsystem();
+  private final ClimberSubsystem m_climberSubsystem = new ClimberSubsystem();
+  private final Vision m_visionSubsystem = new Vision();
+  private final VisionDriveCommand m_visionDriveCommand =
+      new VisionDriveCommand(m_driveSubsystem, m_controller, m_visionSubsystem);
+  private final IndexerSubsystem m_indexerSubsystem = new IndexerSubsystem();
+  private final IntakeSubsystem m_intakeSubsystem = new IntakeSubsystem();
+  private final Compressor compressor =
+      new Compressor(RobotConstants.kREVPH, PneumaticsModuleType.REVPH);
+  VisionTuningCommand m_visionTuningCommand = new VisionTuningCommand(m_visionDriveCommand);
 
-    /**
-     * The container for the robot. Contains subsystems, OI devices, and commands.
-     */
-    public RobotContainer() {
-        // Configure the button bindings
-        configureButtonBindings();
-        configureDefaultCommands();
-        disableCompressor();
-    }
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings();
+    configureDefaultCommands();
+    disableCompressor();
+  }
 
-    /**
-     * Use this method to define your button->command mappings. Buttons can be created by
-     * instantiating a {@link GenericHID} or one of its subclasses
-     * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
-     * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-     */
-    private void configureButtonBindings() {
-        JoystickButton m_driverBButton =
-                new JoystickButton(m_controller, XboxController.Button.kB.value);
-        m_driverBButton.whenHeld(m_visionDriveCommand);
-        JoystickButton m_driverStartButton =
-                new JoystickButton(m_controller, XboxController.Button.kStart.value);
-        m_driverStartButton.whenHeld(m_visionTuningCommand);
-        JoystickButton m_driverLBumper =
-                new JoystickButton(m_controller, XboxController.Button.kLeftBumper.value);
-        m_driverLBumper.whenHeld(new ShooterFullPowerCommand(m_shooterSubsystem)
-                .withTimeout(Constants.AutonConstants.kSpeedUpTime)
-                .andThen(new IndexerFullForwardCommand(m_indexerSubsystem)
-                        .raceWith(new ShooterFullPowerCommand(m_shooterSubsystem))));
-        JoystickButton m_climbJoystickButton =
-                new JoystickButton(m_controller, XboxController.Button.kY.value);
-        m_climbJoystickButton.whenPressed(new ClimberExtendCommand(m_climberSubsystem));
-        JoystickButton m_driverRBumper =
-                new JoystickButton(m_controller, XboxController.Button.kRightBumper.value);
-        m_driverRBumper.whenHeld(new IndexerFullForwardCommand(m_indexerSubsystem));
-        JoystickButton m_driverXButton =
-                new JoystickButton(m_controller, XboxController.Button.kX.value);
-        m_driverXButton.whenHeld(new IntakeCommand(m_intakeSubsystem, m_indexerSubsystem).alongWith(
-                new SequentialCommandGroup(new TopIndexerIntakeCommand(m_indexerSubsystem),
-                        new BottomIndexerIntakeCommand(m_indexerSubsystem))));
-    }
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses
+   * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
+   * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    JoystickButton m_driverBButton =
+        new JoystickButton(m_controller, XboxController.Button.kB.value);
+    m_driverBButton.whenHeld(m_visionDriveCommand);
+    JoystickButton m_driverStartButton =
+        new JoystickButton(m_controller, XboxController.Button.kStart.value);
+    m_driverStartButton.whenHeld(m_visionTuningCommand);
+    JoystickButton m_driverLBumper =
+        new JoystickButton(m_controller, XboxController.Button.kLeftBumper.value);
+    m_driverLBumper.whenHeld(new ShooterFullPowerCommand(m_shooterSubsystem)
+        .withTimeout(Constants.AutonConstants.kSpeedUpTime)
+        .andThen(new IndexerFullForwardCommand(m_indexerSubsystem)
+            .raceWith(new ShooterFullPowerCommand(m_shooterSubsystem))));
+    JoystickButton m_climbJoystickButton =
+        new JoystickButton(m_controller, XboxController.Button.kY.value);
+    m_climbJoystickButton.whenPressed(new ClimberExtendCommand(m_climberSubsystem));
+    JoystickButton m_driverRBumper =
+        new JoystickButton(m_controller, XboxController.Button.kRightBumper.value);
+    m_driverRBumper.whenHeld(new IndexerFullForwardCommand(m_indexerSubsystem));
+    JoystickButton m_driverXButton =
+        new JoystickButton(m_controller, XboxController.Button.kX.value);
+    m_driverXButton.whenHeld(new IntakeCommand(m_intakeSubsystem, m_indexerSubsystem)
+        .alongWith(new SequentialCommandGroup(new TopIndexerIntakeCommand(m_indexerSubsystem),
+            new BottomIndexerIntakeCommand(m_indexerSubsystem))));
+  }
 
-    private void configureDefaultCommands() {
-        m_driveSubsystem.setDefaultCommand(new RunCommand(() -> m_driveSubsystem.arcadeDrive(
-                m_controller.getRightTriggerAxis() - m_controller.getLeftTriggerAxis(),
-                m_controller.getLeftX()), m_driveSubsystem));
-    }
+  private void configureDefaultCommands() {
+    m_driveSubsystem.setDefaultCommand(new RunCommand(() -> m_driveSubsystem.arcadeDrive(
+        m_controller.getRightTriggerAxis() - m_controller.getLeftTriggerAxis(),
+        m_controller.getLeftX()), m_driveSubsystem));
+  }
 
-    public void enableCompressor() {
-        compressor.enableDigital();
-    }
+  public void enableCompressor() {
+    compressor.enableDigital();
+  }
 
-    public void disableCompressor() {
-        compressor.disable();
-    }
+  public void disableCompressor() {
+    compressor.disable();
+  }
 
-    /**
-     * Use this to pass the autonomous command to the main {@link Robot} class.
-     *
-     * @return the command to run in autonomous
-     */
-    public Command getAutonomousCommand() {
-        return createAutoCommand();
-    }
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    return createAutoCommand();
+  }
 
-    public Command createAutoCommand() {
-        return new ShooterFullPowerCommand(m_shooterSubsystem)
-                .withTimeout(Constants.AutonConstants.kSpeedUpTime)
-                .andThen(new IndexerFullForwardCommand(m_indexerSubsystem)
-                        .raceWith(new ShooterFullPowerCommand(m_shooterSubsystem)
-                                .withTimeout(Constants.AutonConstants.kShootTime)))
-                .andThen(new AutonDriveCommand(m_driveSubsystem, 0, .5)
-                        .withTimeout(Constants.AutonConstants.kautonDriveTime));
-    }
+  public Command createAutoCommand() {
+    return new ShooterFullPowerCommand(m_shooterSubsystem)
+        .withTimeout(Constants.AutonConstants.kSpeedUpTime)
+        .andThen(new IndexerFullForwardCommand(m_indexerSubsystem)
+            .raceWith(new ShooterFullPowerCommand(m_shooterSubsystem)
+                .withTimeout(Constants.AutonConstants.kShootTime)))
+        .andThen(new AutonDriveCommand(m_driveSubsystem, 0, .5)
+            .withTimeout(Constants.AutonConstants.kautonDriveTime));
+  }
 }
