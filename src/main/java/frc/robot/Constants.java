cfc7d8dbe70442ee6bef2f6b109c1cc0bf8e76a6
Team OTTO Programming <programming@team1746.com>
2022-03-11 12:14:33 -0500

    shooter speed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -73,7 +73,7 @@ public final class Constants {
     public static final int kShooterSlave2 = 43;
     public static final int kShooterSlave3 = 42;
     public static final int kIndexerMotor = 30;
-    public static final double kFullPower = 0.95;
+    public static final double kFullPower = 0.92;
     public static final int kZeroPower = 0;
     public static final double kLowGoalSpeed = 0.5;
   }
