703209d8a8bed7665c7131fe711ebcd5a4c7139e
Team OTTO Programming <programming@team1746.com>
2022-03-11 13:27:58 -0500

    change branch adding custom rpm and PID

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,85 +15,86 @@ package frc.robot;
  */
 public final class Constants {
 
-  public static class ControllerConstants {
-    public static final int kport = 0;
-    public static final int kport2 = 1;
-    public static final double kdeadZone = .125;
-    public static final double kDriveControl = 2.0;
-  }
+    public static class ControllerConstants {
+        public static final int kport = 0;
+        public static final int kport2 = 1;
+        public static final double kdeadZone = .125;
+        public static final double kDriveControl = 2.0;
+    }
 
-  public static class RobotConstants {
-    public static final int kREVPH = 2;
-  }
+    public static class RobotConstants {
+        public static final int kREVPH = 2;
+    }
 
-  public static class DriveConstants {
-    public static final int kleftDriveLeader = 12;
-    public static final int kleftDriveFollow = 13;
-    public static final int krightDriveLeader = 10;
-    public static final int krightDriveFollow = 11;
-    public static final double kwheelCircumfrence = 4;
-    public static final double kmotorToWheelRatio = 68 / 12;
-  }
+    public static class DriveConstants {
+        public static final int kleftDriveLeader = 12;
+        public static final int kleftDriveFollow = 13;
+        public static final int krightDriveLeader = 10;
+        public static final int krightDriveFollow = 11;
+        public static final double kwheelCircumfrence = 4;
+        public static final double kmotorToWheelRatio = 68 / 12;
+    }
 
-  public static class IntakeConstants {
-    public static final int kIntakeMotor = 20;
-    public static final int kIntakeOmniWheels = 21;
-    public static final int kIntakeSolenoid = 11;
-    public static final int kIntakeFullPower = 1;
-    public static final double kIntakerunZeroPower = 0.0;
-    public static final double kIntakeCustomPower = 0.50;
-    public static final boolean kIntakeExtended = true;
-    public static final boolean kIntakeRetracted = false;
-  }
+    public static class IntakeConstants {
+        public static final int kIntakeMotor = 20;
+        public static final int kIntakeOmniWheels = 21;
+        public static final int kIntakeSolenoid = 11;
+        public static final int kIntakeFullPower = 1;
+        public static final double kIntakerunZeroPower = 0.0;
+        public static final double kIntakeCustomPower = 0.50;
+        public static final boolean kIntakeExtended = true;
+        public static final boolean kIntakeRetracted = false;
+    }
 
-  public static class ClimberConstants {
-    public static final int kMotorR = 51;
-    public static final int kMotorL = 50;
-    public static final double kClimberExtendSpeed = 0.5;
-    public static final double kClimberRetractSpeed = -0.5;
-    public static final int kTopLimitSwitch = 1;
-    public static final int kBottomLimitSwitch = 0;
-    public static final int kChannel = 2;
-    public static final boolean kClimberHookEngaged = true;
-    public static final boolean kClimberHookReleased = false;
-  }
+    public static class ClimberConstants {
+        public static final int kMotorR = 51;
+        public static final int kMotorL = 50;
+        public static final double kClimberExtendSpeed = 0.5;
+        public static final double kClimberRetractSpeed = -0.5;
+        public static final int kTopLimitSwitch = 1;
+        public static final int kBottomLimitSwitch = 0;
+        public static final int kChannel = 2;
+        public static final boolean kClimberHookEngaged = true;
+        public static final boolean kClimberHookReleased = false;
+    }
 
-  public static class AutonConstants {
-    public static final double kautonVelocity = 0.3;
-    public static final double kautonDriveTime = 2.0;
-    public static final double kautonSpeedBackwards = 0.7;
-    public static double kSpeedUpTime = 0.3;
-    public static double kShootTime = 4.0;
-    public static double kDistanceToBall = 25.0;
-  }
+    public static class AutonConstants {
+        public static final double kautonVelocity = 0.3;
+        public static final double kautonDriveTime = 2.0;
+        public static final double kautonSpeedBackwards = 0.7;
+        public static double kSpeedUpTime = 0.3;
+        public static double kShootTime = 4.0;
+        public static double kDistanceToBall = 25.0;
+    }
 
-  public static class ShooterConstants {
-    public static final int kShooterMaster = 41;
-    public static final int kShooterSlave1 = 40;
-    public static final int kShooterSlave2 = 43;
-    public static final int kShooterSlave3 = 42;
-    public static final int kIndexerMotor = 30;
-    public static final double kFullPower = 0.92;
-    public static final int kZeroPower = 0;
-    public static final double kLowGoalSpeed = 0.5;
-  }
+    public static class ShooterConstants {
+        public static final int kShooterMaster = 41;
+        public static final int kShooterSlave1 = 40;
+        public static final int kShooterSlave2 = 43;
+        public static final int kShooterSlave3 = 42;
+        public static final int kIndexerMotor = 30;
+        public static final double kFullPower = 0.92;
+        public static final int kZeroPower = 0;
+        public static final double kLowGoalSpeed = 0.5;
+        public static final double kRPMToTPS = 600 / 4096;
+    }
 
 
 
-  public static class IndexerConstants {
-    public static final double kLowerFullForward = 0.5;
-    public static final double kUpperFullForward = 1;
-    public static final double kLowerHalfForward = 0.5;
-    public static final double kUpperHalfForward = 0.5;
-    public static final double kLowerStop = 0.0;
-    public static final double kUpperStop = 0.0;
-    public static final int kIndexer = 30;
-    public static final int kLower = 30;
-    public static final int kUpper = 31;
-    public static final int kBeamBreakTop = 0;
-    public static final int kBeamBreakBottom = 1;
-    public static final double kLowerCustomPower = 0.5;
-    public static final double kUpperCustomPower = 0.5;
-  }
+    public static class IndexerConstants {
+        public static final double kLowerFullForward = 0.5;
+        public static final double kUpperFullForward = 1;
+        public static final double kLowerHalfForward = 0.5;
+        public static final double kUpperHalfForward = 0.5;
+        public static final double kLowerStop = 0.0;
+        public static final double kUpperStop = 0.0;
+        public static final int kIndexer = 30;
+        public static final int kLower = 30;
+        public static final int kUpper = 31;
+        public static final int kBeamBreakTop = 0;
+        public static final int kBeamBreakBottom = 1;
+        public static final double kLowerCustomPower = 0.5;
+        public static final double kUpperCustomPower = 0.5;
+    }
 
 }
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,26 +1,55 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ShooterConstants;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+
 public class ShooterSubsystem extends SubsystemBase {
-  private VictorSPX master;
-  private TalonSRX slave1;
+  public TalonSRX master;
+  private VictorSPX slave1;
+  public double kP = 0.639375 / 7;
+  public double kF = 0.0;
+  public double kD = 55 * kP;
+  public double kI = 0.0;
+  public double m_RPM;
 
   /** Creates a new ExampleSubsystem. */
   public ShooterSubsystem() {
-    master = new VictorSPX(ShooterConstants.kShooterMaster);
-    slave1 = new TalonSRX(ShooterConstants.kShooterSlave1);
+    master = new TalonSRX(ShooterConstants.kShooterMaster);
+    slave1 = new VictorSPX(ShooterConstants.kShooterSlave1);
 
     slave1.setInverted(false);
     slave1.follow(master);
+    master.config_kF(0, kF);
+    master.config_kP(0, kP);
+    master.config_kI(0, kI);
+    master.config_kD(0, kD);
+  }
+
+  public double getRPM() {
+    return master.getSelectedSensorVelocity() * 600 / 4096;
   }
 
   public void setFullPower() {
     master.set(ControlMode.PercentOutput, ShooterConstants.kFullPower);
+  }
+
+
+
+  @Override
+  public void periodic() {
+
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
   }
 
   public void setCustomPower(double input) {
@@ -31,7 +60,10 @@ public class ShooterSubsystem extends SubsystemBase {
     master.set(ControlMode.PercentOutput, ShooterConstants.kZeroPower);
   }
 
-  public void setLowPower() {
-    master.set(ControlMode.PercentOutput, ShooterConstants.kLowGoalSpeed);
+  public void setRPM(double rpm) {
+    m_RPM = rpm;
+    double velRequest = rpm * ShooterConstants.kRPMToTPS / .6;
+    master.set(ControlMode.Velocity, velRequest);
+
   }
 }
