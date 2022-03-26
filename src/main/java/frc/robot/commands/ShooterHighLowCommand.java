8559bccc66ef991cdff5ed265cbb25a020002a91
Allison Wills <allison.n.wills@gmail.com>
2022-03-26 16:33:02 -0400

    set end method

--- src/main/java/frc/robot/commands/ShooterHighLowCommand.java
+++ src/main/java/frc/robot/commands/ShooterHighLowCommand.java
@@ -38,7 +38,7 @@ public class ShooterHighLowCommand extends CommandBase {
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
-
+        m_shooter.setZeroPower();
     }
 
     // Returns true when the command should end.
