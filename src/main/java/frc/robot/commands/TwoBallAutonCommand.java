1e681eb26caf5b5419cae1b3e05b18e1f2b207b4
afterlxss <2682657-afterlxss@users.noreply.gitlab.com>
2022-03-29 16:02:50 -0400

    drivePID for driving straighter

--- src/main/java/frc/robot/commands/TwoBallAutonCommand.java
+++ src/main/java/frc/robot/commands/TwoBallAutonCommand.java
@@ -10,26 +10,35 @@ import frc.robot.subsystems.IndexerSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.Vision;
+import frc.robot.commands.DriveStraightPIDCommand;
 
 public class TwoBallAutonCommand extends SequentialCommandGroup {
-  public TwoBallAutonCommand(IndexerSubsystem indexerSubsystem, IntakeSubsystem intakeSubsystem,
-      ShooterSubsystem shooterSubsystem, DriveSubsystem driveSubsystem, Vision visionSubsystem,
-      PigeonIMU pigeon) {
-    addCommands(
-        /*
-         * new DriveStraightCommand(driveSubsystem, 6.5, AutonConstants.kautonVelocity), new
-         * SimpleAutonTurningCommand(driveSubsystem, pigeon, 30),
-         */
-        new ParallelRaceGroup(
-            new DriveStraightCommand(driveSubsystem, 9.5, AutonConstants.kautonVelocity),
-            new IntakeCargoCommand(indexerSubsystem, intakeSubsystem)),
-        new DriveStraightCommand(driveSubsystem, -9.5, AutonConstants.kautonVelocity),
-        // new VisionDriveAutonCommand(driveSubsystem, visionSubsystem), //temporary comment
-        // out because we are not testin with vision
-        new ShooterCustomRPMCommand(shooterSubsystem, ShooterConstants.kHighGoalRPM)
-            .withTimeout(AutonConstants.kSpeedUpTime),
-        new ParallelRaceGroup(new IndexerFullForwardCommand(indexerSubsystem),
-            new ShooterCustomRPMCommand(shooterSubsystem, ShooterConstants.kHighGoalRPM)
-                .withTimeout(AutonConstants.kShootTime)));
-  }
+        public TwoBallAutonCommand(IndexerSubsystem indexerSubsystem,
+                        IntakeSubsystem intakeSubsystem, ShooterSubsystem shooterSubsystem,
+                        DriveSubsystem driveSubsystem, Vision visionSubsystem, PigeonIMU pigeon) {
+                addCommands(
+                                /*
+                                 * new DriveStraightCommand(driveSubsystem, 6.5,
+                                 * AutonConstants.kautonVelocity), new
+                                 * SimpleAutonTurningCommand(driveSubsystem, pigeon, 30),
+                                 */
+                                new ParallelRaceGroup(
+                                                new DriveStraightPIDCommand(driveSubsystem, pigeon,
+                                                                9.5, AutonConstants.kautonVelocity),
+                                                new IntakeCargoCommand(indexerSubsystem,
+                                                                intakeSubsystem)),
+                                new DriveStraightCommand(driveSubsystem, -9.5,
+                                                AutonConstants.kautonVelocity),
+                                // new VisionDriveAutonCommand(driveSubsystem, visionSubsystem),
+                                // //temporary comment
+                                // out because we are not testin with vision
+                                new ShooterCustomRPMCommand(shooterSubsystem,
+                                                ShooterConstants.kHighGoalRPM).withTimeout(
+                                                                AutonConstants.kSpeedUpTime),
+                                new ParallelRaceGroup(
+                                                new IndexerFullForwardCommand(indexerSubsystem),
+                                                new ShooterCustomRPMCommand(shooterSubsystem,
+                                                                ShooterConstants.kHighGoalRPM)
+                                                                                .withTimeout(AutonConstants.kShootTime)));
+        }
 }
