7c326f07ad2e0dbf956309da96cafa1b0de06377
Team OTTO Programming <programming@team1746.com>
2022-04-20 18:16:30 -0400

    Fixed some comments and imports in robotcontainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,44 +5,29 @@
 package frc.robot;
 
 import com.ctre.phoenix.sensors.PigeonIMU;
-import com.fasterxml.jackson.databind.deser.std.ThrowableDeserializer;
-import org.ejml.equation.IntegerSequence.Explicit;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.ControllerConstants;
-import frc.robot.Constants.IndexerConstants;
 import frc.robot.commands.ArcadeDriveCommand;
 import frc.robot.Constants.RobotConstants;
-import frc.robot.Constants.ShooterConstants;
 import frc.robot.commands.OneBallAutonCommand;
 import frc.robot.commands.OuttakeCommand;
-import frc.robot.commands.ShooterCustomRPMCommand;
 import frc.robot.commands.ShooterExponentialCommand;
 import frc.robot.commands.AutonDriveCommand;
 import frc.robot.commands.ClimberExtendCommand;
 import frc.robot.commands.ClimberRetractCommand;
 import frc.robot.commands.ClimberStopCommand;
-import frc.robot.commands.FullAutonCommand;
 import frc.robot.commands.HighBarExtendCommand;
 import frc.robot.commands.ReleaseMidBarHook;
 import frc.robot.commands.IndexerFullForwardCommand;
-import frc.robot.commands.IndexerStopCommand;
-import frc.robot.commands.IndexerUpperCommand;
 import frc.robot.commands.IntakeCargoCommand;
-import frc.robot.commands.LowGoalCommand;
 import frc.robot.commands.ShooterFullPowerCommand;
-import frc.robot.commands.TopIndexerIntakeCommand;
-import frc.robot.commands.TwoBallAutonCommand;
 import frc.robot.commands.TwoBallAutonCommand2;
-import frc.robot.commands.ShooterHighLowCommand;
-import frc.robot.commands.ShooterHoodExtendCommand;
-import frc.robot.commands.ShooterHoodRetractCommand;
 import frc.robot.commands.ShooterHoodToggleCommand;
 import frc.robot.commands.VisionDriveCommand;
 import frc.robot.commands.VisionTuningCommand;
@@ -79,17 +64,6 @@ public class RobotContainer {
       new Compressor(RobotConstants.kREVPH, PneumaticsModuleType.REVPH);
   private final VisionTuningCommand m_visionTuningCommand =
       new VisionTuningCommand(m_visionSubsystem, m_driveSubsystem);
-  private final ShooterCustomRPMCommand m_customRPMCommand =
-      new ShooterCustomRPMCommand(m_shooterSubsystem, ShooterConstants.kHighGoalRPM);
-  private final ShooterFullPowerCommand m_shooterFullPower =
-      new ShooterFullPowerCommand(m_shooterSubsystem);
-  private final LowGoalCommand m_lowGoalCommand = new LowGoalCommand(m_shooterSubsystem);
-  private final ShooterHoodExtendCommand m_shooterHoodExtendCommand =
-      new ShooterHoodExtendCommand(m_shooterHoodSubsystem);
-  private final ShooterHoodRetractCommand m_shooterHoodRetractCommand =
-      new ShooterHoodRetractCommand(m_shooterHoodSubsystem);
-  private final ShooterHighLowCommand m_shooterHighLowCommand =
-      new ShooterHighLowCommand(m_shooterHoodSubsystem, m_shooterSubsystem);
   private final PigeonIMU m_pigeon = new PigeonIMU(6);
 
 
@@ -141,52 +115,14 @@ public class RobotContainer {
     xBoxA.toggleWhenPressed(new IntakeCargoCommand(m_indexerSubsystem, m_intakeSubsystem));
     xBoxRBumper2.whenPressed(new ShooterHoodToggleCommand(m_shooterHoodSubsystem));
     xBoxLBumper2.whenHeld(new OuttakeCommand(m_indexerSubsystem, m_intakeSubsystem));
-
-    /*
-     * xBoxLBumper.whenHeld(m_customRPMCommand.withTimeout(Constants.AutonConstants. kSpeedUpTime)
-     * .andThen(new IndexerUpperCommand(m_indexerSubsystem)
-     * .withTimeout(IndexerConstants.kTwoBallDelay) .raceWith( new
-     * ShooterCustomRPMCommand(m_shooterSubsystem, ShooterConstants.kHighGoalRPM)) .andThen(new
-     * IndexerFullForwardCommand(m_indexerSubsystem).raceWith( new
-     * ShooterCustomRPMCommand(m_shooterSubsystem, ShooterConstants.kHighGoalRPM)))));
-     */
-
-
-
     xBoxLBumper.whenHeld(new ShooterExponentialCommand(m_shooterHoodSubsystem, m_shooterSubsystem)
         .raceWith(new IndexerFullForwardCommand(m_indexerSubsystem)));
 
 
-    /*
-     * .andThen(new IndexerUpperCommand(m_indexerSubsystem)
-     * .withTimeout(IndexerConstants.kTwoBallDelay) .raceWith(new
-     * ShooterFullPowerCommand(m_shooterSubsystem)) .andThen(new IndexerFullForwardCommand(m_01
-     * indexerSubsystem) .raceWith(new ShooterFullPowerCommand(m_shooterSubsystem)))));
-     */
-    /**
-     *
-     */
-    /*
-     * xBoxLBumper .whenHeld(m_shooterHighLowCommand.withTimeout(Constants.AutonConstants.
-     * kSpeedUpTime) .andThen(new IndexerUpperCommand(m_indexerSubsystem)
-     * .withTimeout(IndexerConstants.kTwoBallDelay) .raceWith(new
-     * ShooterHighLowCommand(m_shooterHoodSubsystem, m_shooterSubsystem)) .andThen(new
-     * IndexerFullForwardCommand(m_indexerSubsystem).raceWith( new
-     * ShooterHighLowCommand(m_shooterHoodSubsystem, m_shooterSubsystem)))));
-     * 
-     */
 
   }
 
-  // Constants.ShooterConstants.kFullPower - = 1
   private void configureDefaultCommands() {
-    // v1 indexer check
-    // v2 will check for both balls
-    /*
-     * m_indexerSubsystem .setDefaultCommand(new ConditionalCommand(new
-     * IndexerFullForwardCommand(m_indexerSubsystem), new IndexerStopCommand(m_indexerSubsystem),
-     * ShooterSubsystem::getRPMValid));
-     */
 
     m_driveSubsystem.setDefaultCommand(new RunCommand(() -> m_driveSubsystem.arcadeDrive(
         m_controller.getRightTriggerAxis() - m_controller.getLeftTriggerAxis(),
@@ -215,19 +151,6 @@ public class RobotContainer {
 
 
   }
-
-  /*
-   * public Command createAutoCommand() { return new ShooterFullPowerCommand(m_shooterSubsystem)
-   * .withTimeout(Constants.AutonConstants.kSpeedUpTime) .andThen(new
-   * IndexerFullForwardCommand(m_indexerSubsystem) .andThen(new
-   * IntakeExtendCommand(m_intakeSubsystem) .raceWith(new DriveStraightCommand(m_driveSubsystem,
-   * AutonConstants.kDistanceToBall, AutonConstants.kautonVelocity) .andThen(new
-   * DriveStraightCommand(m_driveSubsystem, AutonConstants.kDistanceToBall,
-   * AutonConstants.kautonVelocity)) .andThen(new ShooterFullPowerCommand(m_shooterSubsystem)
-   * .withTimeout(Constants.AutonConstants.kSpeedUpTime) .andThen(new
-   * IndexerFullForwardCommand(m_indexerSubsystem) .raceWith(new
-   * ShooterFullPowerCommand(m_shooterSubsystem))))))); }
-   */
 
   public Command createAutoCommand() {
     return new ShooterFullPowerCommand(m_shooterSubsystem)
