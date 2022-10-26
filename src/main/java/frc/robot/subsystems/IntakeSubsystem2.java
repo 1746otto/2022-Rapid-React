e7521566270fd680324cb01164740878737f86fc
ryvdas <ryvdas@gmail.com>
2022-10-26 17:54:56 -0400

    pain -ryvaan

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem2.java
@@ -0,0 +1,19 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class IntakeSubsystem()
+{
+    CANSparkMax motor;
+    Solenoid pistons;
+    boolean intakeState = false;
+
+  }
+
+  public class IntakeSubsystem()extends
+  subsystemBase() {
+
+}
+
